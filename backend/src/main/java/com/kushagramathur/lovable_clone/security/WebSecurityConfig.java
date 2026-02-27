f4b1fd6aa23666be2b49d2f717a576a46dcbc98b
CoderGhost37 <coderghost37@gmail.com>
2026-02-27 12:32:27 +0530

    feat: frontend setup

--- backend/src/main/java/com/kushagramathur/lovable_clone/security/WebSecurityConfig.java
+++ backend/src/main/java/com/kushagramathur/lovable_clone/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         httpSecurity
                 .csrf(csrfConfig ->csrfConfig.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/webhooks/**").permitAll()
